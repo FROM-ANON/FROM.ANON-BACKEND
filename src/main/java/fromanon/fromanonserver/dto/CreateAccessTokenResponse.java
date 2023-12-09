4583e5834a6705e747f767554965d3d0d02257c2
asaei623 <himtows623@gmail.com>
2023-12-09 17:00:38 +0900

    fix: token 관련 dto 생성

---
+++ src/main/java/fromanon/fromanonserver/dto/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package fromanon.fromanonserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
